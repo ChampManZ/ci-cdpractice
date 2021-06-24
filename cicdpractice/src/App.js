4c1a9650eec2522d3bae2a086bab974604d1c4a3
Thanapat Eurboonyanun <champ_tnt@hotmail.com>
2021-06-24 16:28:23 +0700

    React

---
+++ cicdpractice/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello, World</h1>
+      <button id="clickme">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
